29a524fc27493a853bc74c85f4500a6507515862
adriatic <nik@congral.com>
2021-03-29 21:58:25 -0400

    Migration 2 - added layouts

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,16 @@
+// web/src/pages/AboutPage/AboutPage.js
+
+const AboutPage = () => {
+  return (
+    <>
+      <main>
+        <p>
+          This site was created to demonstrate my mastery of Redwood: Look on my
+          works, ye mighty, and despair!
+        </p>
+      </main>
+    </>
+  )
+}
+
+export default AboutPage
---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,10 @@
+// web/src/pages/HomePage/HomePage.js
+const HomePage = () => {
+  return (
+    <>
+      <main>Home</main>
+    </>
+  )
+}
+
+export default HomePage
